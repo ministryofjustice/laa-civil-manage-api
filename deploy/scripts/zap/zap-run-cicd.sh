d2f2433e52286b90b86df238e81a6c6a6dcdb8d6
Alan Sage <alan.sage@justice.gov.uk>
2026-03-19 08:20:21 +0000

    path

--- deploy/scripts/zap/zap-run-cicd.sh
+++ deploy/scripts/zap/zap-run-cicd.sh
@@ -3,9 +3,8 @@
 echo "--- Updating Zap.Yaml for CI --"
 sed -i 's|zap-results|tmp|g' zap.yaml
 
-cd ../../../
 
 docker compose \
-    -f $(pwd)/deploy/scripts/zap/docker-compose.yml \
+    -f ./docker-compose.yml \
     up \
     --exit-code-from zap-scan

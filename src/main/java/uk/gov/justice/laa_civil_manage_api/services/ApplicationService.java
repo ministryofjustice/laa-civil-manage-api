3d864ea5bbfe595b53e6ad7d376ee8754ce5a6d2
jdwoolcock <30227772+jdwoolcock@users.noreply.github.com>
2026-04-30 13:45:46 +0100

    Connect applications endpoint to data-store api

--- src/main/java/uk/gov/justice/laa_civil_manage_api/services/ApplicationService.java
+++ src/main/java/uk/gov/justice/laa_civil_manage_api/services/ApplicationService.java
@@ -1,55 +1,28 @@
 package uk.gov.justice.laa_civil_manage_api.services;
 
-import java.util.List;
-
 import org.springframework.stereotype.Service;
-
+import uk.gov.justice.laa_civil_manage_api.clients.DataAccessApiClient;
 import uk.gov.justice.laa_civil_manage_api.models.Application;
+
+import java.util.List;
 
 @Service
 public class ApplicationService {
 
-    public Application getApplicationById(String id) {
+    private final DataAccessApiClient dataAccessApiClient;
 
-        return getAllApplications().stream()
-                .filter(app -> app.getApplicationId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new IllegalArgumentException());
+    public ApplicationService(DataAccessApiClient dataAccessApiClient) {
+        this.dataAccessApiClient = dataAccessApiClient;
     }
 
-    private List<Application> getAllApplications() {
-        return List.of(
-                Application.builder()
-                        .applicationId("1")
-                        .clientFirstName("Ali")
-                        .clientLastName("Fletcher")
-                        .status("PENDING")
-                        .build(),
-                Application.builder()
-                        .applicationId("2")
-                        .clientFirstName("Lucas")
-                        .clientLastName("Morrison")
-                        .status("PENDING")
-                        .build(),
-                Application.builder()
-                        .applicationId("3")
-                        .clientFirstName("Denise")
-                        .clientLastName("Bennett")
-                        .status("PENDING")
-                        .build(),
-                Application.builder()
-                        .applicationId("4")
-                        .clientFirstName("Alan")
-                        .clientLastName("Harrington")
-                        .status("PENDING")
-                        .build(),
-                Application.builder()
-                        .applicationId("5")
-                        .clientFirstName("Tom")
-                        .clientLastName("Caldwell")
-                        .status("PENDING")
-                        .build()
+    public List<Application> getApplications() {
+        return dataAccessApiClient.getApplications();
+    }
 
-        );
+    public Application getApplicationById(String id) {
+        return getApplications().stream()
+                .filter(app -> app.applicationId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("Application not found: " + id));
     }
 }

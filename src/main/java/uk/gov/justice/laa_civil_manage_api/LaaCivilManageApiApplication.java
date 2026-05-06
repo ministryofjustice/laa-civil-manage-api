85928c936e0ea8577a72b081ac9037ab4c96cb81
Denise T <denise.turkova@justice.gov.uk>
2026-05-06 14:02:33 +0100

    Created a new expert types  controller (#49)
    * created a new expert types  controller
    
    * added tests
    
    * refactored formatting
    
    * updated openAPI
    
    * chore: removed hardcoded list
    
    * chore: updated tests
    
    * changed config properties
    
    * list tidied up

--- src/main/java/uk/gov/justice/laa_civil_manage_api/LaaCivilManageApiApplication.java
+++ src/main/java/uk/gov/justice/laa_civil_manage_api/LaaCivilManageApiApplication.java
@@ -2,7 +2,9 @@ package uk.gov.justice.laa_civil_manage_api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
 public class LaaCivilManageApiApplication {
 

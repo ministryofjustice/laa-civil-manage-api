645b1268b45f34f756d2e5b61c328c8d5cac2803
aloush-dev <github@aloush.dev>
2026-07-23 12:00:07 +0100

    remove uneeded logs

--- src/main/java/uk/gov/justice/laa_civil_manage_api/services/ApplicationsService.java
+++ src/main/java/uk/gov/justice/laa_civil_manage_api/services/ApplicationsService.java
@@ -2,21 +2,18 @@ package uk.gov.justice.laa_civil_manage_api.services;
 
 import java.util.Collections;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import uk.gov.justice.laa_civil_manage_api.models.ApplicationSummaryResponse;
 import uk.gov.justice.laa_civil_manage_api.models.Paging;
 import uk.gov.justice.laa_civil_manage_api.services.accessdatastore.AccessDataStoreClient;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class ApplicationsService {
 
   private final AccessDataStoreClient accessDataStoreClient;
 
   public ApplicationSummaryResponse getApplicationsData(int page) {
-    log.info("Fetching applications from Data Store via OBO token exchange...");
     ApplicationSummaryResponse response = accessDataStoreClient.getApplications(page);
 
     if (response != null) {
